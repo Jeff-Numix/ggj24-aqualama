6dc8a3e4541ea9f5a7d1e8e435cdb58fb3711ed6
[Numix] Alienware <alienwarre@numix.fr>
2024-01-27 12:21:24 +0100

    integration animations lama

--- Unity/Aqualama/Assets/Scripts/Player.cs
+++ Unity/Aqualama/Assets/Scripts/Player.cs
@@ -6,9 +6,8 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public static Player Instance;
-    private CustomInput input = null;
-    private Vector2 moveVector = Vector2.zero;
     public float moveSpeed=10f;
+    public Animator animator;
 
     [Header("Debug")]
     public bool inputActive=true;
@@ -16,54 +15,39 @@ public class Player : MonoBehaviour
     void Awake()
     {
         Instance=this;
-        input = new CustomInput();
-        
-    }
 
-    void OnEnable()
-    {
-        input.Enable();
-        input.Player.Move.performed  += OnMovementPerformed;
-        input.Player.Move.canceled   += OnMovementCanceled;
-    }
-    
-    void OnDisable()
-    {
-        input.Disable();
-        input.Player.Move.performed  -= OnMovementPerformed;
-        input.Player.Move.canceled   -= OnMovementCanceled;
     }
 
     void Update()
     {
         if(inputActive){
             // Manage horizontal move
-            Vector3 newPosition = transform.position + new Vector3(moveVector.x, 0, 0) * Time.deltaTime * moveSpeed;
+            float moveX = Input.GetAxis("Horizontal");
+            Vector3 newPosition = transform.position + new Vector3(moveX, 0, 0) * Time.deltaTime * moveSpeed;
             Vector3 boundPos = new Vector3(newPosition.x, transform.position.y, 0);
             // Vérifiez si la nouvelle position est à l'intérieur du Collider2D de la zone de déplacement.
             Collider2D moveZone = GameManager.Instance.currentMoveZone;
+            
+            // Disable move if outside Bounds
             if(moveZone!=null){
-                if (moveZone.bounds.Contains(boundPos))
+                if (!moveZone.bounds.Contains(boundPos))
                 {
-                    transform.position = newPosition;
+                    newPosition = transform.position;
                 }
             }
-            else {
+
+            if(newPosition!=transform.position){
                 transform.position = newPosition;
-                Debug.Log("No move zone");
+                animator.SetBool("IsWalking", true);
             }
+            else {
+                animator.SetBool("IsWalking", false);
+            }
+
         }
 
-        
-    }
-
-    private void OnMovementPerformed(InputAction.CallbackContext context)
-    {
-        moveVector = context.ReadValue<Vector2>();
-    }
-
-    private void OnMovementCanceled(InputAction.CallbackContext context)
-    {
-        moveVector = Vector2.zero;
+        if(Input.GetKeyDown(KeyCode.Space)){
+            animator.SetTrigger("Fall");
+        }
     }
 }
